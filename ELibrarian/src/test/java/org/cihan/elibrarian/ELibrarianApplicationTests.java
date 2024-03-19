2c45acd54041ebfe5b44387ea01dff28997abff4
oguzhan.cihan <oguzcihan12@gmail.com>
2024-03-19 18:36:10 +0300

    initial commit

---
+++ ELibrarian/src/test/java/org/cihan/elibrarian/ELibrarianApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cihan.elibrarian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELibrarianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
